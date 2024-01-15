c6b3dec3b2d2d8281f4eacd2f610b05868d8f810
RJ <randal.sartor@gmail.com>
2024-01-15 17:36:57 -0500

    nit: init commit for this account

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link
-  // , animateScroll as scroll 
-} from 'react-scroll';
+import { Link } from 'react-scroll';
 import '../stylesheets/top-nav.css'
 
 export default class Navbar extends React.Component{
